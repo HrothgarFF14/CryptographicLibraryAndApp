b7986799bf30c6cad73eb727857de2496709dba2
Louis Lomboy <llomboy@uw.edu>
2024-05-06 08:56:22 -0700

    created console GUI

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,91 @@
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-        System.out.println("Hello and welcome!");
+        System.out.println("Cryptogtraphy Application by Louis Lomboy, Ahmed Mohamed and Shu-Ren Shen");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Enter mode of operation: " +
+                "\n1-Hash \n2-MAC \n3-Symmetric Encryption \n4-Symmetric Decryption");
+        String mode = scanner.nextLine();
+
+        System.out.println("Enter input method: \n1-File Input \n2-Console Input");
+        String inputMethod = scanner.nextLine();
+
+        System.out.println("Enter output method: \n1-File Output \n2-Console Output");
+        String outputMethod = scanner.nextLine();
+
+        switch (mode) {
+            case "1":
+                // Hash
+                byte[] data = getData(inputMethod);
+                byte[] hash = hashData(data);
+                outputData(hash, outputMethod);
+                break;
+            case "2":
+                // MAC
+                break;
+            case "3":
+                // Symmetric Encryption
+                break;
+            case "4":
+                // Symmetric Decryption
+                break;
+            default:
+                System.out.println("Invalid mode selected.");
+                break;
         }
-        if (args.length < 3) {
-            System.out.println("Please provide the input file, output file and password as arguments.");
-            return;
+
+        scanner.close();
+        System.out.println("---Done!---");
+    }
+
+    private static byte[] getData(String inputMethod) {
+        if ("1".equals(inputMethod)) {
+            System.out.println("Enter file path:");
+            String filePath = scanner.nextLine();
+            try {
+                return Files.readAllBytes(Paths.get(filePath));
+            } catch (IOException e) {
+                System.out.println("Error reading file: " + e.getMessage());
+            }
+        } else if ("2".equals(inputMethod)) {
+            System.out.println("Enter data:");
+            String data = scanner.nextLine();
+            return data.getBytes();
         }
+        return new byte[0];
+    }
 
-        String inputFile = args[0];
-        String outputFile = args[1];
-        String password = args[2];
-
+    private static byte[] hashData(byte[] data) {
         KMACXOF256 kmacxof256 = new KMACXOF256();
+        kmacxof256.KMACXOF256(data, 256);
+        return kmacxof256.finalHash();
+    }
 
-        // Data provided
-        byte[] X = {0x00, 0x01, 0x02, 0x03};
-        int L = 256;
-        String N = "";
-        String S = "Email Signature";
-
-        // Call cSHAKE256 method
-        kmacxof256.cSHAKE256(X, L, N, S);
-
-        // Print the output
-        byte[] output = kmacxof256.finalHash();
-        System.out.println(Arrays.toString(output));
-
-        // Write the output to the outputFile
-        try (FileOutputStream fos = new FileOutputStream(outputFile)) {
-            fos.write(output);
-        } catch (IOException e) {
-            System.out.println("Error writing to file: " + e.getMessage());
+    private static void outputData(byte[] data, String outputMethod) {
+        if ("1".equals(outputMethod)) {
+            System.out.println("Enter output file path:");
+            String filePath = scanner.nextLine();
+            try (FileOutputStream fos = new FileOutputStream(filePath)) {
+                fos.write(data);
+            } catch (IOException e) {
+                System.out.println("Error writing to file: " + e.getMessage());
+            }
+        } else if ("2".equals(outputMethod)) {
+            for (byte b : data) {
+                System.out.print(String.format("%02X ", b));
+            }
+            System.out.println();
         }
-        // Use inputFile, outputFile and password as needed
     }
 }
