af74d2046e89843111827a36a7121a8d2bc52d5f
Shu-Ren Shen <vishen@uw.edu>
2024-06-01 19:04:12 -0700

    add scalarMultiply and sqrt

--- src/Ed448.java
+++ src/Ed448.java
@@ -1,7 +1,9 @@
 import java.math.BigInteger;
 
+import static java.math.BigInteger.ONE;
+
 public class Ed448 {
-    private static final BigInteger p = BigInteger.valueOf(2).pow(448).subtract(BigInteger.valueOf(2).pow(224)).subtract(BigInteger.ONE);
+    private static final BigInteger p = BigInteger.valueOf(2).pow(448).subtract(BigInteger.valueOf(2).pow(224)).subtract(ONE);
     private static final BigInteger d = BigInteger.valueOf(-39081);
 
     private BigInteger x;
@@ -17,15 +19,15 @@ public class Ed448 {
     }
 
     // Constructor for the neutral element (0, 1)
-    public neutralPoint() {
+    public void neutralPoint() {
         this.x = BigInteger.ZERO;
-        this.y = BigInteger.ONE;
+        this.y = ONE;
     }
 
     // Check if the point is on the curve
     private boolean onCurve() {
         BigInteger left = (x.modPow(BigInteger.TWO, p).add(y.modPow(BigInteger.TWO, p)));
-        BigInteger right = BigInteger.ONE.add(d).multiply((x.modPow(BigInteger.TWO, p).add(y.modPow(BigInteger.TWO, p))));
+        BigInteger right = ONE.add(d).multiply((x.modPow(BigInteger.TWO, p).add(y.modPow(BigInteger.TWO, p))));
         return left.equals(right);
     }
 
@@ -49,14 +51,25 @@ public class Ed448 {
         BigInteger x2 = temp.x;
         BigInteger y2 = temp.y;
 
-        BigInteger x3 = (x1.multiply(y2).add(y1.multiply(x2))).divide((BigInteger.ONE).add((d).multiply((x2.multiply(y1).multiply(y2)))));
-        BigInteger y3 = (y1.multiply(y2).subtract((x1.multiply(x2))).divide((BigInteger.ONE).subtract((d).multiply(x1).multiply((x2.multiply(y1).multiply(y2))))));
+        BigInteger x3 = (x1.multiply(y2).add(y1.multiply(x2))).divide((ONE).add((d).multiply((x2.multiply(y1).multiply(y2)))));
+        BigInteger y3 = (y1.multiply(y2).subtract((x1.multiply(x2))).divide((ONE).subtract((d).multiply(x1).multiply((x2.multiply(y1).multiply(y2))))));
 
         return new Ed448(x3, y3);
     }
 
+    public  Ed448 P = new Ed448(BigInteger.ZERO, BigInteger.ZERO);
+    //ToDO  implement P
+
     // Scalar multiplication using the double-and-add algorithm
-    public  scalarMultiply(BigInteger k) {
+    public Ed448 scalarMultiply(BigInteger k) {
+        Ed448 V = P;
+        for (int i = k.bitLength() - 1; i >= 0; i--) {
+            V = V.add(V);
+            if (k.testBit(i)) {
+                V = V.add(P);
+            }
+        }
+        return V;
     }
 
     public BigInteger getX() {
@@ -65,5 +78,27 @@ public class Ed448 {
 
     public BigInteger getY() {
         return y;
+    }
+
+    /**
+     * Compute a square root of v mod p with a specified least-significant bit
+     * if such a root exists.
+     *
+     * @param v the radicand.
+     * @param p the modulus (must satisfy p mod 4 = 3).
+     * @param lsb desired least significant bit (true: 1, false: 0).
+     * @return a square root r of v mod p with r mod 2 = 1 iff lsb = true
+     * if such a root exists, otherwise null.
+     */
+    public static BigInteger sqrt(BigInteger v, BigInteger p, boolean lsb) {
+        assert (p.testBit(0) && p.testBit(1)); // p = 3 (mod 4)
+        if (v.signum() == 0) {
+            return BigInteger.ZERO;
+        }
+        BigInteger r = v.modPow(p.shiftRight(2).add(ONE), p);
+        if (r.testBit(0) != lsb) {
+            r = p.subtract(r); // correct the lsb
+        }
+        return (r.multiply(r).subtract(v).mod(p).signum() == 0) ? r : null;
     }
 }
